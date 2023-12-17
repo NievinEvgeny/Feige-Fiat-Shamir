b64e8b6eba5e3af977656ea6604a49cbd5317f29
NievinEvgeny <zhenya-nievin@mail.ru>
2023-12-17 16:08:06 +0700

    Add template wrap for send and recv calls

--- src/server/libserver/identification.cpp
+++ src/server/libserver/identification.cpp
@@ -23,6 +23,28 @@ void protocol_end(libserver::pthread_data* thread_data)
     delete thread_data;
 }
 
+template <typename T>
+void serv_send(pthread_data* thread_data, T* buf, std::size_t buf_size)
+{
+    if (send(*thread_data->client_sockfd, buf, buf_size, 0) <= 0)
+    {
+        std::cerr << "User disconnected\n";
+        protocol_end(thread_data);
+        pthread_exit(nullptr);
+    }
+}
+
+template <typename T>
+void serv_recv(pthread_data* thread_data, T* buf, std::size_t buf_size)
+{
+    if (recv(*thread_data->client_sockfd, buf, buf_size, 0) <= 0)
+    {
+        std::cerr << "User disconnected\n";
+        protocol_end(thread_data);
+        pthread_exit(nullptr);
+    }
+}
+
 void* identification(void* thread_args)
 {
     auto* thread_data = reinterpret_cast<pthread_data*>(thread_args);
@@ -30,53 +52,27 @@ void* identification(void* thread_args)
     constexpr uint16_t key_size = 8;
     constexpr uint8_t rounds = 5;
 
-    if (send(*thread_data->client_sockfd, &key_size, sizeof(key_size), 0) <= 0)
-    {
-        std::cerr << "User disconnected\n";
-        protocol_end(thread_data);
-        pthread_exit(nullptr);
-    }
+    serv_send(thread_data, &key_size, sizeof(key_size));
+    serv_send(thread_data, &rounds, sizeof(rounds));
 
     bool signup = false;
-
-    if (recv(*thread_data->client_sockfd, &signup, sizeof(signup), 0) <= 0)
-    {
-        std::cerr << "User disconnected\n";
-        protocol_end(thread_data);
-        pthread_exit(nullptr);
-    }
-
     uint8_t login_size = 0;
 
-    if (recv(*thread_data->client_sockfd, &login_size, sizeof(login_size), 0) <= 0)
-    {
-        std::cerr << "User disconnected\n";
-        protocol_end(thread_data);
-        pthread_exit(nullptr);
-    }
+    serv_recv(thread_data, &signup, sizeof(signup));
+    serv_recv(thread_data, &login_size, sizeof(login_size));
 
     std::string login(login_size, '\0');
 
-    if (recv(*thread_data->client_sockfd, login.data(), login_size, 0) <= 0)
-    {
-        std::cerr << "User disconnected\n";
-        protocol_end(thread_data);
-        pthread_exit(nullptr);
-    }
+    serv_recv(thread_data, login.data(), login_size);
 
-    std::vector<int64_t> client_shared_key;
-    client_shared_key.reserve(key_size + 1);
-    client_shared_key.resize(key_size + 1);
+    std::vector<int64_t> shared_key;
+    shared_key.reserve(key_size + 1);
+    shared_key.resize(key_size + 1);
     constexpr std::size_t shared_key_size = (key_size + 1) * sizeof(int64_t);
 
     if (signup)
     {
-        if (recv(*thread_data->client_sockfd, client_shared_key.data(), shared_key_size, 0) <= 0)
-        {
-            std::cerr << "User disconnected\n";
-            protocol_end(thread_data);
-            pthread_exit(nullptr);
-        }
+        serv_recv(thread_data, shared_key.data(), shared_key_size);
 
         std::fstream database("serverDB.csv", std::ios::binary | std::ios::app | std::ios::in | std::ios::out);
         if (!database.is_open())
@@ -114,7 +110,7 @@ void* identification(void* thread_args)
         database.write(reinterpret_cast<const char*>(&login_size), sizeof(login_size));
         database.write(login.c_str(), login_size);
 
-        for (const auto& element : client_shared_key)
+        for (const auto& element : shared_key)
         {
             database.write(reinterpret_cast<const char*>(&element), sizeof(element));
         }
@@ -141,7 +137,7 @@ void* identification(void* thread_args)
 
             if (tmp_login == login)
             {
-                database.read(reinterpret_cast<char*>(client_shared_key.data()), shared_key_size);
+                database.read(reinterpret_cast<char*>(shared_key.data()), shared_key_size);
                 break;
             }
 
@@ -155,19 +151,7 @@ void* identification(void* thread_args)
             pthread_exit(nullptr);
         }
 
-        if (send(*thread_data->client_sockfd, &client_shared_key.back(), sizeof(client_shared_key.back()), 0) <= 0)
-        {
-            std::cerr << "User disconnected\n";
-            protocol_end(thread_data);
-            pthread_exit(nullptr);
-        }
-    }
-
-    if (send(*thread_data->client_sockfd, &rounds, sizeof(rounds), 0) <= 0)
-    {
-        std::cerr << "User disconnected\n";
-        protocol_end(thread_data);
-        pthread_exit(nullptr);
+        serv_send(thread_data, &shared_key.back(), sizeof(shared_key.back()));
     }
 
     bool protocol_result = true;
@@ -176,30 +160,15 @@ void* identification(void* thread_args)
     {
         int64_t rand_num = 0;
 
-        if (recv(*thread_data->client_sockfd, &rand_num, sizeof(rand_num), 0) <= 0)
-        {
-            std::cerr << "User disconnected\n";
-            protocol_end(thread_data);
-            pthread_exit(nullptr);
-        }
+        serv_recv(thread_data, &rand_num, sizeof(rand_num));
 
         uint8_t rand_bits = utils::gen_bits();
 
-        if (send(*thread_data->client_sockfd, &rand_bits, sizeof(rand_bits), 0) <= 0)
-        {
-            std::cerr << "User disconnected\n";
-            protocol_end(thread_data);
-            pthread_exit(nullptr);
-        }
+        serv_send(thread_data, &rand_bits, sizeof(rand_bits));
 
         int64_t masked_secret = 0;
 
-        if (recv(*thread_data->client_sockfd, &masked_secret, sizeof(masked_secret), 0) <= 0)
-        {
-            std::cerr << "User disconnected\n";
-            protocol_end(thread_data);
-            pthread_exit(nullptr);
-        }
+        serv_recv(thread_data, &masked_secret, sizeof(masked_secret));
 
         int64_t secret_check = 1;
 
@@ -207,33 +176,25 @@ void* identification(void* thread_args)
         {
             if (rand_bits & (1 << i))
             {
-                secret_check = utils::mod(secret_check * client_shared_key.at(i), client_shared_key.back());
+                secret_check = utils::mod(secret_check * shared_key.at(i), shared_key.back());
             }
         }
 
-        secret_check = utils::mod(secret_check * rand_num, client_shared_key.back());
+        secret_check = utils::mod(secret_check * rand_num, shared_key.back());
 
-        if ((secret_check == 0)
-            || (secret_check != std::abs(utils::pow_mod(masked_secret, 2, client_shared_key.back()))))
+        if ((secret_check == 0) || (secret_check != std::abs(utils::pow_mod(masked_secret, 2, shared_key.back()))))
         {
             protocol_result = false;
+            serv_send(thread_data, &protocol_result, sizeof(protocol_result));
             std::cerr << "Client not approved\n";
-            protocol_end(thread_data);
-            pthread_exit(nullptr);
-        }
 
-        if (send(*thread_data->client_sockfd, &protocol_result, sizeof(protocol_result), 0) <= 0)
-        {
-            std::cerr << "User disconnected\n";
             protocol_end(thread_data);
             pthread_exit(nullptr);
         }
     }
 
-    if (protocol_result)
-    {
-        std::cout << "Client approved\n";
-    }
+    serv_send(thread_data, &protocol_result, sizeof(protocol_result));
+    std::cout << "Client approved\n";
 
     protocol_end(thread_data);
     pthread_exit(nullptr);
